6c526cd9d3f95a90998be7ca2c09b13cbdfe7cd0
Ahmed Fouad <ahmedfouad2020era@gmail.com>
2018-12-20 23:43:26 +0200

    Remove Turbolink

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,5 +13,4 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
