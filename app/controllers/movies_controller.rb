2131d2efccef82caafeaf92762a0bf30f795f8ae
Ahmed Fouad <ahmedfouad2020era@gmail.com>
2018-12-20 21:31:34 +0200

    Add search feature, pagination, home page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,23 @@ class MoviesController < ApplicationController
   def index
     if params[:page].blank? 
       @genres = Tmdb::Movie.popular
-    elsif 
+    else
       @genres = Tmdb::Movie.popular(page: params[:page])
+    end
+  end
+
+  def home
+  end
+  
+  def search_results
+    if params[:page].blank? && params[:name].blank?
+      redirect_to trendingmovies_path
+    elsif params[:page].blank?
+      @searchedname = params[:name]
+      @moviesnames = Tmdb::Search.movie(@searchedname)
+    else
+      @searchedname = params[:name]
+      @moviesnames = Tmdb::Search.movie(@searchedname, page: params[:page])
     end
   end
 end
