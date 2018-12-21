5e8199e50efafa3c46638a12d389b5f9cf68684c
Ahmed Fouad <ahmedfouad2020era@gmail.com>
2018-12-21 15:03:44 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get '/moviedetails',     to: 'movies#movie_details'
  
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
